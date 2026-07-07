71bc5e8e0fad8c67b6d8d3332fbd2675e75e7284
Pavithra <p71212764@gmail.com>
2026-07-07 22:42:23 +0530

    Time: 2 ms (40.15%) | Memory: 12.5 MB (25.87%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def numberToWords(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "Zero"
+
+        below20 = [
+            "", "One", "Two", "Three", "Four", "Five",
+            "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen",
+            "Fifteen", "Sixteen", "Seventeen",
+            "Eighteen", "Nineteen"
+        ]
+
+        tens = [
+            "", "", "Twenty", "Thirty", "Forty",
+            "Fifty", "Sixty", "Seventy",
+            "Eighty", "Ninety"
+        ]
+
+        def helper(n):
+            if n == 0:
+                return ""
+            elif n < 20:
+                return below20[n] + " "
+            elif n < 100:
+                return tens[n // 10] + " " + helper(n % 10)
+            elif n < 1000:
+                return below20[n // 100] + " Hundred " + helper(n % 100)
+            elif n < 1000000:
+                return helper(n // 1000) + "Thousand " + helper(n % 1000)
+            elif n < 1000000000:
+                return helper(n // 1000000) + "Million " + helper(n % 1000000)
+            else:
+                return helper(n // 1000000000) + "Billion " + helper(n % 1000000000)
+
+        return helper(num).strip()
