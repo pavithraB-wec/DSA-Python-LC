2bc10eb830dbf9f481c5b2e6254512fc0f25090a
Pavithra <p71212764@gmail.com>
2026-07-07 22:31:52 +0530

    Time: 74 ms (59.90%) | Memory: 19.1 MB (57.61%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        rev = s[::-1]
+        temp = s + "#" + rev
+
+        lps = [0] * len(temp)
+
+        j = 0
+
+        for i in range(1, len(temp)):
+            while j > 0 and temp[i] != temp[j]:
+                j = lps[j - 1]
+
+            if temp[i] == temp[j]:
+                j += 1
+                lps[i] = j
+
+        longest = lps[-1]
+
+        return rev[:len(s) - longest] + s
