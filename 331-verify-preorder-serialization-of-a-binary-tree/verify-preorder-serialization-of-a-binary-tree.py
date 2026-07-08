1b8acd29d404bdca264a628188fd6270df321bfb
Pavithra <p71212764@gmail.com>
2026-07-08 14:17:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.33%) - LeetSync

---
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+        slots = 1
+
+        for node in preorder.split(','):
+            # Every node uses one slot
+            slots -= 1
+
+            if slots < 0:
+                return False
+
+            # Non-null node creates two new slots
+            if node != '#':
+                slots += 2
+
+        return slots == 0
