ecb4823b849b57167e2834a1c3ee899f9aa90f24
Pavithra <p71212764@gmail.com>
2026-07-10 09:56:58 +0530

    Time: 445 ms (54.35%) | Memory: 12.5 MB (71.74%) - LeetSync

---
+++ 664-strange-printer/strange-printer.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def strangePrinter(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = 1
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = dp[i + 1][j] + 1
+
+                for k in range(i + 1, j + 1):
+                    if s[i] == s[k]:
+                        if k == i + 1:
+                            dp[i][j] = min(dp[i][j], dp[k][j])
+                        else:
+                            dp[i][j] = min(dp[i][j], dp[i + 1][k - 1] + dp[k][j])
+
+        return dp[0][n - 1]
