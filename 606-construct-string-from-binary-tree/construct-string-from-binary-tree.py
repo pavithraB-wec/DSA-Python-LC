5c646098ac8909a0c76d72e7d074639fb1a4bc39
Pavithra <p71212764@gmail.com>
2026-07-10 09:39:01 +0530

    Time: 1 ms (94.67%) | Memory: 15.2 MB (81.33%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def tree2str(self, root):
+        """
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+
+        if not root.left and not root.right:
+            return str(root.val)
+
+        if not root.right:
+            return str(root.val) + "(" + self.tree2str(root.left) + ")"
+
+        return (str(root.val) +
+                "(" + self.tree2str(root.left) + ")" +
+                "(" + self.tree2str(root.right) + ")")
