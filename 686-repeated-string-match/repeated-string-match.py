dcfd9a72759780804873027899ba4853a97dc7f7
Pavithra <p71212764@gmail.com>
2026-07-10 11:42:36 +0530

    Time: 79 ms (49.42%) | Memory: 12.5 MB (48.84%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def repeatedStringMatch(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        repeated = a
+        count = 1
+
+        while len(repeated) < len(b):
+            repeated += a
+            count += 1
+
+        if b in repeated:
+            return count
+
+        repeated += a
+        if b in repeated:
+            return count + 1
+
+        return -1
