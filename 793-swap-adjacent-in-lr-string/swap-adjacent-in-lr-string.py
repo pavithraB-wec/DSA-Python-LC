b5ff77d64281d6ae4e7e529607fe655780816605
Pavithra <p71212764@gmail.com>
2026-07-11 23:17:37 +0530

    Time: 8 ms (95.71%) | Memory: 12.2 MB (100.00%) - LeetSync

---
+++ 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def canTransform(self, start, result):
+        """
+        :type start: str
+        :type result: str
+        :rtype: bool
+        """
+        if start.replace("X", "") != result.replace("X", ""):
+            return False
+
+        n = len(start)
+        i = j = 0
+
+        while i < n and j < n:
+            while i < n and start[i] == 'X':
+                i += 1
+            while j < n and result[j] == 'X':
+                j += 1
+
+            if i == n or j == n:
+                break
+
+            if start[i] != result[j]:
+                return False
+
+            if start[i] == 'L' and i < j:
+                return False
+
+            if start[i] == 'R' and i > j:
+                return False
+
+            i += 1
+            j += 1
+
+        return True
