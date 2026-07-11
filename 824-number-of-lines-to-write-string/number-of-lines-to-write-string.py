0cfdfa7e676a1ed7ebb432deb190cb8ac160d5a1
Pavithra <p71212764@gmail.com>
2026-07-11 23:26:23 +0530

    Time: 18 ms (49.28%) | Memory: 12.3 MB (89.86%) - LeetSync

---
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numberOfLines(self, widths, s):
+        """
+        :type widths: List[int]
+        :type s: str
+        :rtype: List[int]
+        """
+        lines = 1
+        current_width = 0
+
+        for ch in s:
+            w = widths[ord(ch) - ord('a')]
+
+            if current_width + w > 100:
+                lines += 1
+                current_width = w
+            else:
+                current_width += w
+
+        return [lines, current_width]
