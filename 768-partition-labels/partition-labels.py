e57500cdedbd7ad8f0ca3eed625d9da35dd4e8a1
Pavithra <p71212764@gmail.com>
2026-07-11 23:08:17 +0530

    Time: 3 ms (94.43%) | Memory: 12.5 MB (19.48%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        last = {}
+
+        for i, ch in enumerate(s):
+            last[ch] = i
+
+        result = []
+        start = 0
+        end = 0
+
+        for i, ch in enumerate(s):
+            end = max(end, last[ch])
+
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+
+        return result
