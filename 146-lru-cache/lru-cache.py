ca5bef74ef77ae8c1ab286b97fe1ad1659ad6c47
Pavithra <p71212764@gmail.com>
2026-07-12 20:39:26 +0530

    Time: 317 ms (38.90%) | Memory: 78.4 MB (51.69%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,69 @@
+class Node(object):
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = {}
+
+        # Dummy head and tail
+        self.head = Node()
+        self.tail = Node()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self._remove(node)
+        self._insert(node)
+
+        return node.value
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.cache:
+            self._remove(self.cache[key])
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self._insert(node)
+
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
