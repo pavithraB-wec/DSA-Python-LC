a2539f3627b1176e31b91abf0bc0291f4d355df5
Pavithra <p71212764@gmail.com>
2026-07-14 21:04:08 +0530

    Time: 87 ms (38.46%) | Memory: 13.4 MB (43.96%) - LeetSync

---
+++ 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.py
@@ -0,0 +1,30 @@
+from collections import Counter, defaultdict
+
+class Solution(object):
+    def isPossible(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        freq = Counter(nums)
+        need = defaultdict(int)
+
+        for num in nums:
+            if freq[num] == 0:
+                continue
+
+            freq[num] -= 1
+
+            if need[num] > 0:
+                need[num] -= 1
+                need[num + 1] += 1
+
+            elif freq[num + 1] > 0 and freq[num + 2] > 0:
+                freq[num + 1] -= 1
+                freq[num + 2] -= 1
+                need[num + 3] += 1
+
+            else:
+                return False
+
+        return True
