097efc0abaf9285eb7876ac129aad96d35687fb1
Pavithra <p71212764@gmail.com>
2026-07-17 19:08:13 +0530

    Time: 273 ms (16.66%) | Memory: 14.8 MB (27.78%) - LeetSync

---
+++ 991-array-of-doubled-pairs/array-of-doubled-pairs.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution(object):
+    def canReorderDoubled(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count = Counter(arr)
+
+        for x in sorted(arr, key=abs):
+            if count[x] == 0:
+                continue
+
+            if count[2 * x] == 0:
+                return False
+
+            count[x] -= 1
+            count[2 * x] -= 1
+
+        return True
