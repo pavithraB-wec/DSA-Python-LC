327c8f4201d375c00652b5c8c74138a1f36c1cb8
Pavithra <p71212764@gmail.com>
2026-07-21 20:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (12.75%) - LeetSync

---
+++ 233-number-of-digit-one/number-of-digit-one.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        factor = 1
+
+        while factor <= n:
+            lower = n % factor
+            current = (n // factor) % 10
+            higher = n // (factor * 10)
+
+            if current == 0:
+                count += higher * factor
+            elif current == 1:
+                count += higher * factor + lower + 1
+            else:
+                count += (higher + 1) * factor
+
+            factor *= 10
+
+        return count
