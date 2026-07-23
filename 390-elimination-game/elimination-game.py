25f9c25001029fb42a2448af98f804c39950d2ac
Pavithra <p71212764@gmail.com>
2026-07-23 10:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (62.21%) - LeetSync

---
+++ 390-elimination-game/elimination-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lastRemaining(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        head = 1
+        step = 1
+        left = True
+        remaining = n
+
+        while remaining > 1:
+            if left or remaining % 2 == 1:
+                head += step
+
+            remaining //= 2
+            step *= 2
+            left = not left
+
+        return head
