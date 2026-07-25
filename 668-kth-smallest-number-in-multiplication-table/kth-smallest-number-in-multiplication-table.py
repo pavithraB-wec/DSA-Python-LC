6fa73bd14b83c1afc05379ddc5d6f047fb44ea3d
Pavithra <p71212764@gmail.com>
2026-07-25 11:49:01 +0530

    Time: 543 ms (82.89%) | Memory: 13.4 MB (5.26%) - LeetSync

---
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findKthNumber(self, m, n, k):
+        """
+        :type m: int
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        left, right = 1, m * n
+
+        while left < right:
+            mid = (left + right) // 2
+
+            count = 0
+            for i in range(1, m + 1):
+                count += min(n, mid // i)
+
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
