61909bfa9daccb0f6a555ffc80336cfa2b3db807
Pavithra <p71212764@gmail.com>
2026-07-25 12:14:17 +0530

    Time: 3 ms (25.97%) | Memory: 12.3 MB (59.74%) - LeetSync

---
+++ 738-monotone-increasing-digits/monotone-increasing-digits.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def monotoneIncreasingDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        digits = list(str(n))
+        marker = len(digits)
+
+        for i in range(len(digits) - 1, 0, -1):
+            if digits[i] < digits[i - 1]:
+                digits[i - 1] = str(int(digits[i - 1]) - 1)
+                marker = i
+
+        for i in range(marker, len(digits)):
+            digits[i] = '9'
+
+        return int("".join(digits))
