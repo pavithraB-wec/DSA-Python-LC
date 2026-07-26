181d821e30301c3c9e78b9bd287d79151a05d846
Pavithra <p71212764@gmail.com>
2026-07-26 15:26:01 +0530

    Time: 362 ms (53.98%) | Memory: 12.3 MB (85.47%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,32 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if len(s) < 2:
-            return s
+        if not s:
+            return ""
 
         start = 0
-        maxLen = 1
+        end = 0
 
         def expand(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return left + 1, right - 1
+            return right - left - 1
 
         for i in range(len(s)):
+            len1 = expand(i, i)
+            len2 = expand(i, i + 1)
+            length = max(len1, len2)
 
-            # Odd length palindrome
-            l1, r1 = expand(i, i)
+            if length > end - start + 1:
+                start = i - (length - 1) // 2
+                end = i + length // 2
 
-            # Even length palindrome
-            l2, r2 = expand(i, i + 1)
-
-            if r1 - l1 + 1 > maxLen:
-                start = l1
-                maxLen = r1 - l1 + 1
-
-            if r2 - l2 + 1 > maxLen:
-                start = l2
-                maxLen = r2 - l2 + 1
-
-        return s[start:start + maxLen]
+        return s[start:end + 1]
