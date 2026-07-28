08b09842dba93be83b80f64d23ba8c3fcf3366d0
Pavithra <p71212764@gmail.com>
2026-07-28 12:35:25 +0530

    Time: 780 ms (93.66%) | Memory: 12.8 MB (87.44%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return -1 if dp[amount] == amount + 1 else dp[amount]
