7cba5b778bf76188f13140757a3bd2a6a144249c
Pavithra <p71212764@gmail.com>
2026-08-12 20:05:42 +0530

    Time: 4 ms (40.00%) | Memory: 12.6 MB (5.76%) - LeetSync

---
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def largestSumAfterKNegations(self, nums, k):
+        nums.sort()
+
+        # Flip negative numbers first
+        for i in range(len(nums)):
+            if k > 0 and nums[i] < 0:
+                nums[i] = -nums[i]
+                k -= 1
+
+        # If odd flips remain, flip the smallest value
+        if k % 2 == 1:
+            nums.sort()
+            nums[0] = -nums[0]
+
+        return sum(nums)
