b745ef683f7e7389d4f8f977d9b5e053d51b6ce8
Pavithra <p71212764@gmail.com>
2026-08-20 18:38:34 +0530

    Time: 64 ms (12.86%) | Memory: 18.3 MB (35.36%) - LeetSync

--- 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
@@ -3,32 +3,27 @@ class NestedIterator(object):
     def __init__(self, nestedList):
         self.stack = []
 
-        # Push in reverse order
+        # Push elements in reverse order
         for item in reversed(nestedList):
             self.stack.append(item)
 
     def next(self):
-        # hasNext() makes sure the top is an integer
         self.hasNext()
-
-        item = self.stack.pop()
-        return item.getInteger()
+        return self.stack.pop().getInteger()
 
     def hasNext(self):
         while self.stack:
             item = self.stack[-1]
 
-            # Top is already an integer
+            # If it is an integer, we are ready
             if item.isInteger():
                 return True
 
-            # Top is a list
+            # Remove the nested list
             self.stack.pop()
 
-            nested = item.getList()
-
-            # Push its elements in reverse order
-            for child in reversed(nested):
+            # Add its elements in reverse order
+            for child in reversed(item.getList()):
                 self.stack.append(child)
 
         return False
